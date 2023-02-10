ee46bd9fbb45118cf6a3fa59049de6854bd9f1d3
kotru <88907641+kotru21@users.noreply.github.com>
2023-02-11 00:24:06 +0300

    repository clear

--- src/js/index/NavbarScroll.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    var $bx = $(".bx");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    $bx.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-  });
-});
